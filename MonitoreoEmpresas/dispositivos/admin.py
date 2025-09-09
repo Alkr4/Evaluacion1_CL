bbb3a12c7c230b71e60fe5b29fe572de354134e2
Cristopher <cris.al.lat.bar@gmail.com>
2025-09-09 16:13:18 -0300

    configuraciones admin para app dispositivos y usuarios

--- MonitoreoEmpresas/dispositivos/admin.py
+++ MonitoreoEmpresas/dispositivos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Organization, Category, Zone, Device, Measurement, Alert
 # Register your models here.
+
+admin.site.register(Organization)
+admin.site.register(Category)
+admin.site.register(Zone)
+admin.site.register(Device)
+admin.site.register(Measurement)
+admin.site.register(Alert)
